c8c6637c3938d0ae918c32bc1fb8fd845c69a3e0
Jovana Bulatovic <bulatovicj07@gmail.com>
2021-08-30 16:32:21 +0200

    Kreiranje strane Area i dizajna i sitne ispravke.

--- js/script.js
+++ js/script.js
@@ -16,3 +16,58 @@ anime.timeline({loop: true})
     easing: "easeOutExpo",
     delay: 1000
   });
+
+/*line stranica*/
+var lineGenerator = d3.line();
+var points = [
+	[0, 80],
+	[100, 100],
+	[200, 30],
+	[300, 50],
+	[400, 40],
+	[500, 80]
+];
+
+var pathData = lineGenerator(points);
+
+//selektovanje putanje i postavljanej d atrbuta
+d3.select('path')
+	.attr('d', pathData);
+
+ /* area 1 kod */
+var areaGenerator = d3.area();
+
+  var point = [
+    [0, 80],
+    [100, 100],
+    [200, 30],
+    [300, 50],
+    [400, 40],
+    [500, 80]
+  ];
+  
+  var area = areaGenerator(point);
+  // kreiranje putanje elemnta i postavljanje d atributa
+   d3.select('g')
+  .append('path')
+  .attr('d', area);
+
+
+ /* area 2 kod */
+  var areaGenerator2 = d3.area().y0(150);
+  var points = [
+    [0, 80],
+    [100, 100],
+    [200, 30],
+    [300, 50],
+    [400, 40],
+    [500, 80]
+  ];
+  
+  var area = areaGenerator2(points);
+  
+  // kreiranje putanje elemnta i postavljanje d atributa
+  d3.select('#area2')
+    .append('path')
+    .attr('d', area);
+    
